4021dcd322f71e15296297fb462874477c82adb2
Joseph-kaku <samkaks18@gmail.com>
2022-11-10 11:26:57 -0700

    updates

--- js/index.js
+++ js/index.js
@@ -8,86 +8,25 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
-class Sprite
-{
-    constructor({position})
-    {
-        this.position = position
-        this.width = 50
-        this.height = 150
-        
-    }
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
 
-    draw() {}
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 128
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
 
-
-    update() {
-        this.draw()
-    }
-}
-
-class Fighter
-{
-    constructor({position, velocity, color = 'red', offset})
-    {
-        this.position = position
-        this.velocity = velocity
-        this.width = 50
-        this.height = 150
-        this.lastkey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset, 
-            width: 100,
-            height: 50,
-        }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
-
-    draw()
-    {
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        // attack box
-        if (this.isAttacking) {
-        c.fillStyle = 'blue'
-        c.fillRect(this.attackBox.position.x, 
-            this.attackBox.position.y, 
-            this.attackBox.width, 
-            this.attackBox.height)
-        }
-    }
-
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if(this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else{
-            this.velocity.y += gravity
-        }
-    }
-
-    attack() {
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100);
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
 position: {
     x:0,
     y:0
@@ -99,12 +38,19 @@ velocity: {
 offset: {
     x:0,
     y:0
+},
+imageSrc: './img/samuraiMack/Idle.png',
+framesMax: 8,
+scale: 2.5,
+offset: {
+    x: 215,
+    y: 157
 }
 })
 
 
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x:400,
         y:100
@@ -144,52 +90,16 @@ const keys = {
     
 }
 
-function rectangularCollison({rectangle1, rectangle2}) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width 
-        >= rectangle2.position.x && rectangle1.attackBox.position.x 
-        <= rectangle2.position.x + rectangle2.width
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height 
-        >= rectangle2.position.y && rectangle1.attackBox.position.y 
-        <= rectangle2.position.y + rectangle2.height
-    )
-}
-
-function determineWinner({player, enemy, timerId}) {
-    clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-    if(player.health == enemy.health) 
-    {
-        document.querySelector('#displayText').innerHTML = 'TIE'
-    } else if (player.health > enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Player 1 wins'
-    } else {
-        document.querySelector('#displayText').innerHTML = 'Player 2 wins'
-    }
-}
-
-let timer = 60
-let timerId
-function decreaseTimer() {
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-    if (timer == 0) 
-    {
-        determineWinner({player, enemy, timerId})
-    }
-}
-
 decreaseTimer();
 
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
+    shop.update()
     player.update()
-    enemy.update()
+    // enemy.update()
 
     player.velocity.x = 0
     enemy.velocity.x = 0
