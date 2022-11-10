e48e446c14cf3865e94cbaecfcd56fd0cc959d1f
Joseph-kaku <samkaks18@gmail.com>
2022-11-09 21:02:03 -0700

    basic functionality done

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class Sprite
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw()
@@ -36,13 +37,13 @@ class Sprite
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
         // attack box
-        // if (this.isAttacking) {
+        if (this.isAttacking) {
         c.fillStyle = 'blue'
         c.fillRect(this.attackBox.position.x, 
             this.attackBox.position.y, 
             this.attackBox.width, 
             this.attackBox.height)
-        // }
+        }
     }
 
     update() {
@@ -127,12 +128,43 @@ const keys = {
 
 function rectangularCollison({rectangle1, rectangle2}) {
     return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= 
-        rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width 
+        >= rectangle2.position.x && rectangle1.attackBox.position.x 
+        <= rectangle2.position.x + rectangle2.width
+        && rectangle1.attackBox.position.y + rectangle1.attackBox.height 
+        >= rectangle2.position.y && rectangle1.attackBox.position.y 
+        <= rectangle2.position.y + rectangle2.height
     )
 }
+
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health == enemy.health) 
+    {
+        document.querySelector('#displayText').innerHTML = 'TIE'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 wins'
+    } else {
+        document.querySelector('#displayText').innerHTML = 'Player 2 wins'
+    }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if (timer == 0) 
+    {
+        determineWinner({player, enemy, timerId})
+    }
+}
+
+decreaseTimer();
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -165,21 +197,37 @@ function animate() {
     }
 
     // detect collision
-    if (player.attackBox.position.x + player.attackBox.width >= 
-        enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width
-        && player.attackBox.position.y + player.attackBox.height >= enemy.position.y
-        && player.attackBox.position.y <= enemy.position.y + enemy.height 
+    if ( rectangularCollison({
+        rectangle1: player,
+        rectangle2: enemy
+    })
         && player.isAttacking) {
             player.isAttacking = false
-        console.log('go');
+            enemy.health -= 20
+            document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
+
+    if ( rectangularCollison({
+        rectangle1: enemy,
+        rectangle2: player
+    })
+        && enemy.isAttacking) {
+            enemy.isAttacking = false
+            player.health -= 20
+            document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+
+// end game
+if (enemy.health <= 0 || player.health <= 0){
+    determineWinner({player, enemy, timerId})
+}
 }
 
 animate()
 
 
 window.addEventListener('keydown', (event) => {
-    console.log(event.key);
     switch(event.key){
         case 'd':
             keys.d.pressed = true
@@ -206,9 +254,11 @@ window.addEventListener('keydown', (event) => {
             break
         case 'ArrowUp':
             enemy.velocity.y = -20
-            break        
+            break 
+        case 'ArrowDown':
+                enemy.isAttacking = true
+                break              
     }
-    console.log(event.key)
 
 })
 
@@ -231,6 +281,5 @@ switch(event.key){
         keys.ArrowLeft.pressed = false
         break   
     }
-    console.log(event.key)
 
 })
